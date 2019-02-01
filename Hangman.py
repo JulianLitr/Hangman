eb9e5892be28f93ffd372af5f0fe45c17ebd6060
Julian G. Litrento <46292859+JulianLitr@users.noreply.github.com>
2019-02-01 17:07:44 -0500

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,23 +1,51 @@
 #Hangman with preset word
 
-word = "apple"
-guesses = 10
-print("Let's play Hangman. You have 10 Guesses to guess the word")
-while guesses > 0:
-    guesses = 10
+word = list("apple")
+strWord = "apple"
+guesses = 3
+winner = False
+incorrectLetters = []
+guess = [""] * len(word)
+print(guess)
+def wrong():
+    global guesses
+    guesses -= 1
+
+def score():
+    print("Guesses left: " + str(guesses))
+    print("Incorrect Letters: ", end ="")
+    for x in range(len(incorrectLetters)):
+        if x == len(incorrectLetters) - 1:
+            print(incorrectLetters[x], end ="")
+        else:
+            print(incorrectLetters[x], end =", ")
+    print("")
+    for x in range(len(guess)):
+        if x == len(guess) - 1:
+            print(guess[x], end ="_")
+        else:
+            print(guess[x], end ="_,")
+    print("")
+            
+def ask():
+    letter = input("Please guess a letter: ")
+    #letterguess = letter
+    for i in range(len(strWord)):
+        if letter == word[i]:
+            print("")
+            
+            
+
     
-    print("You have " + str(guesses) + " left!")
-    guess = input("Guess a letter: \n")
-  
-    if guess not in word:        
-        print("There's no " + guess + ".")
-        guesses -= 1
+print("Let's play Hangman. You have 10 Guesses to guess the word")
+while guesses > 0 and winner == False:
+    score()
 
+    ask()
+    
+    guesses -= 1
 
-    elif guess == word:
-        print("Correct! The answer was apple!")
-        break
-    elif guess in word:
-        print("Correct")
-    else:
-        print("You are out of guesses :(")
+if guesses == 0:
+    print("You lost. The word is " + strWord)
+else:
+    print("Great job! You won with " + str(guesses) + " guesses left!")
