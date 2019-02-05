7b04957ffaea18d8b191441e18aa3bb4516659ef
Julian G. Litrento <46292859+JulianLitr@users.noreply.github.com>
2019-02-05 12:04:18 -0500

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -2,17 +2,17 @@
 
 word = list("apple")
 strWord = "apple"
-guesses = 3
+guesses = 5
 winner = False
 incorrectLetters = []
-guess = [""] * len(word)
-print(guess)
+guess = ["_"] * len(word)
+
 def wrong():
     global guesses
     guesses -= 1
 
 def score():
-    print("Guesses left: " + str(guesses))
+    print("\n\nGuesses left: " + str(guesses))
     print("Incorrect Letters: ", end ="")
     for x in range(len(incorrectLetters)):
         if x == len(incorrectLetters) - 1:
@@ -22,30 +22,44 @@ def score():
     print("")
     for x in range(len(guess)):
         if x == len(guess) - 1:
-            print(guess[x], end ="_")
+            print(guess[x], end ="")
         else:
-            print(guess[x], end ="_,")
+            print(guess[x], end =",")
     print("")
             
 def ask():
-    letter = input("Please guess a letter: ")
-    #letterguess = letter
-    for i in range(len(strWord)):
+    letter = input("Please guess a letter: ").lower()
+    correct = False
+    for i in range(len(word)):
         if letter == word[i]:
-            print("")
+            guess[i] = letter
+            correct = True
+    if correct:
+        print(letter + " is a correct letter.")
+    else:
+        print("There is no " + letter)
+        global guesses 
+        guesses -= 1
+        incorrectLetters.append(letter)
             
+def check():
+    if "_" not in guess:
+        global winner
+        winner = True
             
-
-    
+                
 print("Let's play Hangman. You have 10 Guesses to guess the word")
 while guesses > 0 and winner == False:
     score()
 
     ask()
     
-    guesses -= 1
+    check()
 
 if guesses == 0:
     print("You lost. The word is " + strWord)
 else:
-    print("Great job! You won with " + str(guesses) + " guesses left!")
+    if guesses == 1:
+        print("Great job! You won with " + str(guesses) + " guess left!")
+    else:
+        print("Great job! You won with " + str(guesses) + " guesses left!")
